d32a9017bf68b33f7defcf3a3feb7056177663e7
Haruto <haruto_tanaka_0323@icloud.com>
2024-10-17 23:21:47 +0900

    仮骨格まで完成

---
+++ haruto_affi_poikatsu_killer/src/components/pointsites/pointsite001.tsx
@@ -0,0 +1,11 @@
+const PointSite001: React.FC = () => {
+    return (
+        <div>
+            <h1> ポイントサイトの名前 </h1>
+            <p> ポイントサイトの概要・おすすめポイント</p>
+            <a>ポイントサイト（アフィの案件）のリンク</a>
+        </div>
+    );
+};
+
+export default PointSite001;
