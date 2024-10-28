7924f108abea153746c3a04c87486fa00208d98a
yamaguchi <aidukaibuki810@gmail.com>
2024-10-28 11:34:07 +0900

    add homepage, general css

--- haruto_affi_poikatsu_killer/src/components/articles/Article001.tsx
+++ haruto_affi_poikatsu_killer/src/components/articles/Article001.tsx
@@ -2,7 +2,7 @@ const Article001: React.FC = () => {
     return (
         <div>
             <h3> 記事タイトル001 </h3>
-            <a>記事のリンク001</a>
+            <a className="border">記事のリンク001</a>
         </div>
     );
 };
